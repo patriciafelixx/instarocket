36b9ceb9dc177863ee80aee270c014e56d53755b
unknown <patriciafelixx@outlook.com>
2020-08-17 11:49:55 -0300

    Interface Web

---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import api from '../services/api';
+
+import './Feed.css';
+import more from '../assets/more.svg'
+import like from '../assets/like.svg'
+import comment from '../assets/comment.svg'
+import send from '../assets/send.svg'
+
+class Feed extends Component {
+    state = {
+        feed: []
+    }
+
+    async componentDidMount() {
+        const response = await api.get('posts');
+        this.setState({ feed: response.data });
+    }
+
+    handleLike = id => {
+        api.post(`/posts/${id}/like`);
+    }
+
+    render() {
+        return (
+            <section id="post-list">
+                {this.state.feed.map(post => (
+                    <article key={post._id}>
+                        <header>
+                            <div class="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
+
+                            <img src={more} alt="Mais" />
+                        </header>
+
+                        <img src={`http://localhost:3333/files/${post.image}`} />
+
+                        <footer>
+                            <div className="actions">
+                                <button type="button">
+                                    <img src={like} alt="" onClick={() => this.handleLike(post._id)} />
+                                </button>
+                                <img src={comment} alt="" />
+                                <img src={send} alt="" />
+                            </div>
+                            <strong>{post.likes} curtidas</strong>
+                            <p>
+                                {post.description}
+                            <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                ))}
+            </section>
+        )
+    }
+}
+
+export default Feed;
